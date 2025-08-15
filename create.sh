8b96747d94e24a85f62f781b66c43934cfc24aba
Chris Kopec <c_kopec@hotmail.com>
2025-08-15 14:59:07 -0400

    Updates to create script

--- create.sh
+++ create.sh
@@ -1,10 +1,11 @@
-
+#!/bin/bash
 
 echo "Reading first line from list."
 
 # Specify the input file
 INPUT_FILE="list.txt"
 LANGUAGE="python"
+FOLDER="Python"
 
 # Get first line from input
 LINE=$(head -n 1 $INPUT_FILE)
@@ -12,12 +13,14 @@ echo "Processing line: $LINE"
 
 # Create the prompt
 PROMPT="Show an example of $LINE algorithm in $LANGUAGE, in raw markdown."
-echo "$PROMPT"
+echo "Prompt: $PROMPT"
 
 # Generate ollama result
-#ollama run qwen3-coder $PROMPT > "$LINE.md"
+OUTPUT_PATH="./$FOLDER/$LINE.md"
+echo "Output path: $OUTPUT_PATH"
+echo "Using ollama to generate output."
+#ollama run qwen3-coder $PROMPT > $OUTPUT_PATH
 
-echo "hello world" > $LINE.md
 
 
 
